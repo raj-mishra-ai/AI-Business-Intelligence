84f854cb821c124084c4276235976810f04eb510
Raj Mishra <rajmishra.ai01@gmail.com>
2026-08-29 15:47:24 +0530

    Updated API URL and CORS

--- app/main.py
+++ app/main.py
@@ -31,14 +31,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# CORS Configuration
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 logger.info("Application Started Successfully")
 
